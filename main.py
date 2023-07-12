6d87770cd3abd13c521658cc461b44fc689aa4ab
xabhishekhar <72898173+xabhishekhar@users.noreply.github.com>
2023-07-12 10:21:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,9 +45,9 @@ async def message_handler(event):
         if  not await get_user_join(event.sender_id):
             haha = await event.reply(f'''**Hey! {event.sender.first_name} 😃**
 
-**join our updates channel to use me ✅**
+**Please Join Our Updates Channel To Use Me ✅**
 
-**Click on this button 👇🏻 to join**''', buttons=Button.url('🍿Updates Channel🍿', f'https://t.me/{Config.UPDATES_CHANNEL_USERNAME}'))
+**Click Here To To Join👇👇**''', buttons=Button.url('👉Updates Channel👈', f'https://t.me/{Config.UPDATES_CHANNEL_USERNAME}'))
             await asyncio.sleep(Config.AUTO_DELETE_TIME)
             return await haha.delete()
 
